2f85b3157fee49705980b7a734661d1fdfc77799
rainswerld <m.rainrobinson@gmail.com>
2020-10-06 15:28:29 -0600

    Update location component to include date

--- src/Location/Location.js
+++ src/Location/Location.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 function Location() {
+  const dateSet = (today) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    let day = days[today.getDay()]
+    let date = today.getDate()
+    let month = months[today.getMonth()]
+    let year = today.getFullYear()
+
+    return `${day} ${date} ${month}, ${year}`
+  }
+
   return (
-    <div>
-      I am the location
+    <div className="location-box">
+      <div className="location">
+        Example City, Place
+      </div>
+      <div className="date">{dateSet(new Date())}</div>
     </div>
   )
 }
