70167e351d71d96113dc778c162d1a9960c0620e
rainswerld <m.rainrobinson@gmail.com>
2020-10-07 10:38:46 -0600

    Updates

---
+++ precache-manifest.f1ff07dbe6c0f3f904f85a06d3e55609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f952370bc1a89c2740461cf194d718",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e55f867e6fdc615e48c0",
+    "url": "/weather-app/static/css/main.096641ca.chunk.css"
+  },
+  {
+    "revision": "468e94502b3f1026426f",
+    "url": "/weather-app/static/js/2.9f1533dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.9f1533dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55f867e6fdc615e48c0",
+    "url": "/weather-app/static/js/main.c44c7d62.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
